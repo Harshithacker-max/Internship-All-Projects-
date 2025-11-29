a0a6d6eb100ff71ef1f389cbfb20d161ea4ad0da
Harshithacker-max <kumarankit5639op@gmail.com>
2025-11-29 13:09:30 +0530

    Add comments for speech recognition functionality
    Added comments explaining the purpose of the speech recognition code.

--- SpeechRecognitation.py
+++ SpeechRecognitation.py
@@ -1,4 +1,5 @@
-
+# I am using the concept of speech recoganization which is teach by jibrran ali sir & i also use pywhat.kit and make a you tube speech recoganization when you speak any thing which you want to play on youtube 
+#just run the code and simply said anthything which you want to play on youtube & it's play that video 
 import speech_recognition as sr
 import pyttsx3
 speaker = pyttsx3.init()
